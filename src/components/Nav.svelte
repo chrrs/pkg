434712f4c411ba6cfd0eb50909f9d53d71bd8130
Chris <chirssel@gmail.com>
2021-12-17 17:52:54 +0100

    Seperate the navbar into a separate component

---
+++ src/components/Nav.svelte
@@ -0,0 +1,14 @@
+<div class="py-4 bg-white">
+	<div class="flex items-center gap-8 max-w-3xl mx-auto">
+		<a href="/">
+			<img src="/logo.svg" alt="pkg" class="h-8" />
+		</a>
+		<div class="flex flex-grow">
+			<input
+				class="flex-grow bg-gray-100 px-4 py-2 border-2 border-r-0 border-transparent focus:border-black focus:outline-none"
+				placeholder="Search maven artifacts..."
+			/>
+			<button class="bg-black text-white px-6 font-bold">Search</button>
+		</div>
+	</div>
+</div>
