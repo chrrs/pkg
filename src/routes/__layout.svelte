c852d36953c8cb856590483225f7dc49a4bc4e9b
Chris <chirssel@gmail.com>
2021-12-17 17:41:40 +0100

    Go back to the home page when clicking on the logo

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,7 +4,9 @@
 
 <div class="py-4 bg-white">
 	<div class="flex items-center gap-8 max-w-3xl mx-auto">
-		<img src="/logo.svg" alt="pkg" class="h-8" />
+		<a href="/">
+			<img src="/logo.svg" alt="pkg" class="h-8" />
+		</a>
 		<div class="flex flex-grow">
 			<input
 				class="flex-grow bg-gray-100 px-4 py-2 border-2 border-r-0 border-transparent focus:border-black focus:outline-none"
