194c98a6a7625dad54d6ed149aa918b1b30bef93
Chris <chirssel@gmail.com>
2021-12-17 18:09:24 +0100

    Navigate to the search page when using the search bar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,14 +1,26 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	let query = '';
+
+	function search() {
+		goto(`/search?q=${encodeURIComponent(query)}`);
+		query = '';
+	}
+</script>
+
 <div class="py-4 bg-white">
 	<div class="flex items-center gap-8 max-w-3xl mx-auto">
 		<a href="/">
 			<img src="/logo.svg" alt="pkg" class="h-8" />
 		</a>
-		<div class="flex flex-grow">
+		<form on:submit|preventDefault={search} class="flex flex-grow">
 			<input
 				class="flex-grow bg-gray-100 px-4 py-2 border-2 border-r-0 border-transparent focus:border-black focus:outline-none"
 				placeholder="Search maven artifacts..."
+				bind:value={query}
 			/>
-			<button class="bg-black text-white px-6 font-bold">Search</button>
-		</div>
+			<button type="submit" class="bg-black text-white px-6 font-bold">Search</button>
+		</form>
 	</div>
 </div>
