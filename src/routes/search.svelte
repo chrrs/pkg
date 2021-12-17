7aa289af3ddc745041043d000dd7c9f6591514e3
Chris <chirssel@gmail.com>
2021-12-17 20:36:12 +0100

    Use the search endpoint to fill in search results

--- src/routes/search.svelte
+++ src/routes/search.svelte
@@ -1,9 +1,19 @@
 <script lang="ts" context="module">
 	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
-	export async function load({ page }: LoadInput): Promise<LoadOutput> {
+	export async function load({ page, fetch }: LoadInput): Promise<LoadOutput> {
 		if (page.query.has('q')) {
+			const query = page.query.get('q');
+			const res = (await fetch(`/api/search?q=${encodeURIComponent(query)}`).then((res) =>
+				res.json()
+			)) as SearchResults & ApiError;
+
+			if (res.error) {
+				// TODO: Handle this more gracefully.
+				throw res.error;
+			}
+
 			return {
-				props: { query: page.query.get('q') },
+				props: { query, results: res },
 			};
 		} else {
 			return {
@@ -15,7 +25,11 @@
 </script>
 
 <script lang="ts">
+	import type { ApiError, SearchResults } from '$lib/api_types';
+	import { formatDistance } from 'date-fns';
+
 	export let query: string;
+	export let results: SearchResults;
 </script>
 
 <svelte:head>
@@ -25,20 +39,22 @@
 <div class="bg-gray-100 py-4 mb-4">
 	<div class="flex justify-between max-w-3xl mx-auto">
 		<p class="font-semibold">Search results for <span class="italic">{query}</span></p>
-		<p>28 results</p>
+		<p>{results.amount} results</p>
 	</div>
 </div>
 
 <div class="max-w-[50rem] mx-auto">
-	<a href="/pkg/net.kyori:adventure-api" class="block hover:bg-gray-100 px-4 py-4">
-		<p class="text-lg flex gap-4 mb-1">
-			<span class="font-semibold text-purple-700">adventure-api</span>
-			<span>Platform integrations for the adventure UI library</span>
-		</p>
-		<p class="text-sm text-gray-500 flex gap-4">
-			<span class="text-gray-800">net.kyori:adventure-api</span>
-			<span class="text-green-700 font-semibold">v4.0.0</span>
-			5 months ago
-		</p>
-	</a>
+	{#each results.results as result}
+		<a href="/pkg/{result.group}:{result.artifact}" class="block hover:bg-gray-100 px-4 py-4">
+			<p class="text-lg flex gap-4 mb-1">
+				<span class="font-semibold text-purple-700">{result.name}</span>
+				{#if result.description}<span>{result.description}</span>{/if}
+			</p>
+			<p class="text-sm text-gray-500 flex gap-4">
+				<span class="text-gray-800">{result.group}:{result.artifact}</span>
+				<span class="text-green-700 font-semibold">v{result.version}</span>
+				{formatDistance(result.lastUpdated, new Date(), { addSuffix: true })}
+			</p>
+		</a>
+	{/each}
 </div>
