b29783f3320c77544b09974e254c34f2dbb65e46
Chris <chirssel@gmail.com>
2021-12-17 17:17:25 +0100

    Add initial design

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,7 @@
 <script>
 	import '../app.css';
 </script>
+
+<div class="w-screen h-screen">
 	<slot />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="py-4 bg-white">
+	<div class="flex items-center gap-8 max-w-3xl mx-auto">
+		<img src="/logo.svg" alt="pkg" class="h-8" />
+		<div class="flex flex-grow">
+			<input
+				class="flex-grow bg-gray-100 px-4 py-2 border-2 border-r-0 border-transparent focus:border-black focus:outline-none"
+				placeholder="Search maven artifacts..."
+			/>
+			<button class="bg-black text-white px-6 font-semibold">Search</button>
+		</div>
+	</div>
+</div>
+
+<div class="bg-gray-100 py-4 mb-4">
+	<div class="flex justify-between max-w-3xl mx-auto">
+		<p class="font-semibold">Search results for <span class="italic">adventure</span></p>
+		<p>28 results</p>
+	</div>
+</div>
+
+<div class="max-w-[50rem] mx-auto">
+	<a href="/pkg/net.kyori:adventure-api" class="block hover:bg-gray-100 px-4 py-4">
+		<p class="text-lg flex gap-4 mb-1">
+			<span class="font-semibold text-purple-700">adventure-api</span>
+			<span>Platform integrations for the adventure UI library</span>
+		</p>
+		<p class="text-sm text-gray-500 flex gap-4">
+			<span class="text-gray-800">net.kyori:adventure-api</span>
+			<span class="text-green-700 font-semibold">v4.0.0</span>
+			5 months ago
+		</p>
+	</a>
+</div>
