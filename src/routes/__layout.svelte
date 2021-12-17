eb2dda468202c3a19fbbdc31c6aa0de9e3084cc6
Chris <chirssel@gmail.com>
2021-12-17 17:27:04 +0100

    Move header into the global layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,19 @@
 	import '../app.css';
 </script>
 
+<div class="py-4 bg-white">
+	<div class="flex items-center gap-8 max-w-3xl mx-auto">
+		<img src="/logo.svg" alt="pkg" class="h-8" />
+		<div class="flex flex-grow">
+			<input
+				class="flex-grow bg-gray-100 px-4 py-2 border-2 border-r-0 border-transparent focus:border-black focus:outline-none"
+				placeholder="Search maven artifacts..."
+			/>
+			<button class="bg-black text-white px-6 font-bold">Search</button>
+		</div>
+	</div>
+</div>
+
 <div class="w-screen h-screen">
 	<slot />
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,3 @@
-<div class="py-4 bg-white">
-	<div class="flex items-center gap-8 max-w-3xl mx-auto">
-		<img src="/logo.svg" alt="pkg" class="h-8" />
-		<div class="flex flex-grow">
-			<input
-				class="flex-grow bg-gray-100 px-4 py-2 border-2 border-r-0 border-transparent focus:border-black focus:outline-none"
-				placeholder="Search maven artifacts..."
-			/>
-			<button class="bg-black text-white px-6 font-bold">Search</button>
-		</div>
-	</div>
-</div>
-
 <div class="bg-gray-100 py-4 mb-4">
 	<div class="flex justify-between max-w-3xl mx-auto">
 		<p class="font-semibold">Search results for <span class="italic">adventure</span></p>
