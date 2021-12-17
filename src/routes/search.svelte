5f2239abd0552f9888583fb414c4306b3f5ceb1e
Chris <chirssel@gmail.com>
2021-12-17 17:27:15 +0100

    Move the search page to /search

---
+++ src/routes/search.svelte
@@ -0,0 +1,20 @@
+<div class="bg-gray-100 py-4 mb-4">
+	<div class="flex justify-between max-w-3xl mx-auto">
+		<p class="font-semibold">Search results for <span class="italic">adventure</span></p>
+		<p>28 results</p>
+	</div>
+</div>
+
+<div class="max-w-[50rem] mx-auto">
+	<a href="/pkg/net.kyori:adventure-api" class="block hover:bg-gray-100 px-4 py-4">
+		<p class="text-lg flex gap-4 mb-1">
+			<span class="font-semibold text-purple-700">adventure-api</span>
+			<span>Platform integrations for the adventure UI library</span>
+		</p>
+		<p class="text-sm text-gray-500 flex gap-4">
+			<span class="text-gray-800">net.kyori:adventure-api</span>
+			<span class="text-green-700 font-semibold">v4.0.0</span>
+			5 months ago
+		</p>
+	</a>
+</div>
