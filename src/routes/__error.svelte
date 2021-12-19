9d395a111bba8b13729cbebe5d80a974ccba0772
Chris <chirssel@gmail.com>
2021-12-19 19:48:06 +0100

    Customize the error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').ErrorLoad} */
+	export function load({ error, status }) {
+		return {
+			props: {
+				status: status,
+				message: error.message,
+			},
+		};
+	}
+</script>
+
+<script lang="ts">
+	import Alert from '$components/Alert.svelte';
+
+	export let status: number;
+	export let message: string;
+</script>
+
+<div class="max-w-3xl mx-auto mb-8">
+	<Alert type="error"
+		><b>Error {status}:</b>
+		{message}
+	</Alert>
+</div>
