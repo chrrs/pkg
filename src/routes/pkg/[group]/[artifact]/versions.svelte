e93d66d0dceae067a563f7f8e8c2b70997c13b42
Chris <chirssel@gmail.com>
2021-12-25 15:40:39 +0100

    Add first pass of versions page

---
+++ src/routes/pkg/[group]/[artifact]/versions.svelte
@@ -0,0 +1,33 @@
+<script lang="ts" context="module">
+	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
+	export async function load({ stuff }: LoadInput): Promise<LoadOutput> {
+		return {
+			props: stuff,
+		};
+	}
+</script>
+
+<script lang="ts">
+	import type { Package } from '$lib/api_types';
+	import { formatDistance } from 'date-fns';
+
+	export let pack: Package;
+</script>
+
+<div class="max-w-[50rem] mx-auto mb-8">
+	{#each pack.versions as version}
+		<button class="w-full px-4 py-4 flex items-center gap-4 group hover:bg-gray-100">
+			<p
+				class="text-lg font-semibold"
+				class:text-green-700={version.stable}
+				class:text-red-700={!version.stable}
+			>
+				v{version.name}
+			</p>
+			<div class="flex-grow border-b-2 border-gray-100 group-hover:border-gray-200" />
+			<p class="text-gray-500">
+				{formatDistance(version.updated, new Date(), { addSuffix: true })}
+			</p>
+		</button>
+	{/each}
+</div>
