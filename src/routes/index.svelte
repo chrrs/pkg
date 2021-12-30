8dd6793ef5a29a8f164eff659a4cc9620bd657fd
Chris <chirssel@gmail.com>
2021-12-30 10:51:32 +0100

    Add a search URL shortcut

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,20 @@
+<script lang="ts" context="module">
+	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
+	export async function load({ page }: LoadInput): Promise<LoadOutput> {
+		let query = '';
+		page.query.forEach((_, q) => (query += q));
+
+		if (query !== '') {
+			return {
+				status: 303,
+				redirect: `/search?q=${query}`,
+			};
+		}
+
+		return {};
+	}
+</script>
+
 <svelte:head>
 	<title>pkg</title>
 </svelte:head>
